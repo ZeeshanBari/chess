c27f233a82e7e49c70b5537c4e863ac821c247e8
ZeeshanBari <zeeshan.bari13@gmail.com>
2024-12-24 23:08:16 -0500

    test homepage update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test Homepage
         </a>
       </header>
     </div>
